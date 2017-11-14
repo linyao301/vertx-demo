8981026a3d7a0d854eb5721c8e30ee32a84278f8
z1213572724@qq.com <z1021010246>
2017-11-14 09:34:06 +0800

    整合ICE

--- my-vertx-demo/src/main/java/com/test/entity/package-info.java
+++
@@ -1,4 +0,0 @@
-@ModuleGen(name="test-entity",groupPackage = "com.test.entity")
-package com.test.entity;
-
-import io.vertx.codegen.annotations.ModuleGen;
---
+++ vertx-blue-book/src/main/java/com/vertx/verticle/MyVerticle.java
@@ -0,0 +1,4 @@
+package com.vertx.verticle;
+
+public class MyVerticle {
+}
